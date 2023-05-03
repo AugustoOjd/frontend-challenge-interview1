1ad788d1876d1fe8f04cab9a008f273be8110688
Augusto Ojeda <okarinex@gmail.com>
2023-05-03 19:16:49 -0300

    test cors credentials v1

--- src/api/apiShoes.ts
+++ src/api/apiShoes.ts
@@ -2,26 +2,15 @@ import axios from "axios";
 import Cookies from 'js-cookie'
 
 export const apiShoes = axios.create({
+    withCredentials: true,
     baseURL: 'http://localhost:8080/api/shoe/',
-    headers: {
-        xtoken: Cookies.get('xtoken'),
-    },
-    withCredentials: true
   });
 
 export const apiBrands = axios.create({
     baseURL: 'http://localhost:8080/api/brand/',
-    headers: {
-        xtoken: Cookies.get('xtoken'),
-    },
-    withCredentials: true
   });
 
 export const apiAuth = axios.create({
     baseURL: 'http://localhost:8080/api/auth/',
-    headers: {
-        xtoken: Cookies.get('xtoken'),
-    },
-    withCredentials: true
 })
 
